ed4a864bcceeb5ceafad8cefc62c7829aa0dcb2b
Maribel Duran <maribelduranucm@gmail.com>
2018-08-01 19:33:30 -0700

    Update json url link

--- web/js/index.js
+++ web/js/index.js
@@ -1,163 +1,173 @@
 /************* MODEL ************/
 var app = {
   needs: {},
-  initNeeds: function(){
+  initNeeds: function() {
     this.needs = new Needs();
   }
 };
 
-function Needs(){
- this.needs = {};
+function Needs() {
+  this.needs = {};
 }
 
-Needs.prototype.addNeeds = function(list){
+Needs.prototype.addNeeds = function(list) {
   //build a map of key  => count entries
-  list.reduce(function(map, needs){
-  if (map[needs]){
-    map[needs]++;
-  }else{
-    map[needs]=1;
-  }
-  return map;
- }.bind(this), this.needs);
-}
+  console.log(this.needs);
+  list.reduce(
+    function(map, needs) {
+      if (map[needs]) {
+        map[needs]++;
+      } else {
+        map[needs] = 1;
+      }
+      return map;
+    }.bind(this),
+    this.needs
+  );
+  console.log(this.needs);
+};
 
 //sorts needs based on the entry counts from highest to lowest and returns the keys in an array
-Needs.prototype.sortNeeds = function(){
-  var sorted = Object.keys(this.needs).sort(function(a,b){
-    return this.needs[b] - this.needs[a];
-  }.bind(this));
+Needs.prototype.sortNeeds = function() {
+  console.log(Object.keys(this.needs));
+  var sorted = Object.keys(this.needs).sort(
+    function(a, b) {
+      return this.needs[b] - this.needs[a];
+    }.bind(this)
+  );
+
   return sorted;
-}
+};
 
 /************* CONTROLLER ************/
 var controller = {
-  initNeeds: function(){
+  initNeeds: function() {
     app.initNeeds();
     this.getNeeds();
   },
-  getNeeds: function(){
-    var url = 'http://www.maribelduran.com/Hacking4Humanity2017/web/js/ShelterDictionary.JSON';
+  getNeeds: function() {
+    var url =
+      "https://maribelduran.github.io//Hacking4Humanity2017/web/js/ShelterDictionary.JSON";
     var request = new XMLHttpRequest();
-    request.open('GET', url, true);
+    request.open("GET", url, true);
     request.onload = function() {
-    if (request.status >= 200 && request.status < 400) { 
-      var shelters = JSON.parse(request.response);
-      shelters.forEach(function(obj){
-      var needList = obj["needs"];
-      app.needs.addNeeds(needList);
-      });
-    }else{
-       // We reached our target server, but it returned an error   
-    }
-    var sortedNeeds = app.needs.sortNeeds();
-    view.showNeeds(sortedNeeds);
-  };
-  request.onerror = function() {
+      if (request.status >= 200 && request.status < 400) {
+        var shelters = JSON.parse(request.response);
+        shelters.forEach(function(obj) {
+          var needList = obj["needs"];
+          app.needs.addNeeds(needList);
+        });
+      } else {
+        // We reached our target server, but it returned an error
+      }
+      var sortedNeeds = app.needs.sortNeeds();
+      view.showNeeds(sortedNeeds);
+    };
+    request.onerror = function() {
       // There was a connection error of some sort
     };
     request.send();
   }
 };
 
-
 /************* VIEW ************/
 var view = {
   //object to lookup icon for each need
-  
   svgIcon: {
     "baby supplies": "infant-supplies.svg",
     "baby wipes": "baby-wipes.svg",
     "bart tickets": "bart.svg",
-    "blankets": "blankets.svg",
-    "clothing": "shirt.svg",
-    "diapers": "diaper.svg",
+    blankets: "blankets.svg",
+    clothing: "shirt.svg",
+    diapers: "diaper.svg",
     "feminine hygiene": "tampon.svg",
-    "furniture": "sofa.svg",
+    furniture: "sofa.svg",
     "gift cards": "gift-card.svg",
     "laundry detergent": "detergent.svg",
-    "money": "money.svg",
-    "notepads": "notepad.svg",
-    "pens": "pen.svg",
-    "socks": "sock.svg",
-    "soap": "soap.svg",
+    money: "money.svg",
+    notepads: "notepad.svg",
+    pens: "pen.svg",
+    socks: "sock.svg",
+    soap: "soap.svg",
     "sticky notes": "sticky-notes.svg",
     "toilet paper": "toilet-paper.svg",
     "toothbrush/paste": "toothbrush.svg",
-    "towels": "towel.svg",
-    "underwear": "underwear.svg"
+    towels: "towel.svg",
+    underwear: "underwear.svg"
   },
+  //Creates html elements to show needs in bootstrap rows.
+  showNeeds: function(needs) {
+    //displays top three needs in a separate bootstrap row
+    var topThreeNeeds = document.getElementById("top-three-needs");
+    topThreeNeeds.innerHTML = "";
 
-//Creates html elements to show needs in bootstrap rows.
- showNeeds: function(needs){
-  //displays top three needs in a separate bootstrap row
-  var topThreeNeeds = document.getElementById("top-three-needs");
-  topThreeNeeds.innerHTML = "";
+    //displays the rest of the needs in another bootstrap row
+    var restofNeeds = document.getElementById("rest-of-needs");
+    restofNeeds.innerHTML = "";
 
-  //displays the rest of the needs in another bootstrap row
-  var restofNeeds = document.getElementById("rest-of-needs");
-  restofNeeds.innerHTML = "";
+    needs.forEach(
+      function(item, index) {
+        //title case the item name
+        var name = titleCase(item);
 
-  needs.forEach(function(item, index){
-    //title case the item name
-    var name = titleCase(item);
+        //top three needs go in 3 columns
+        if (index <= 2) {
+          var col_md_4 = document.createElement("div");
+          col_md_4.className = "col-md-4";
+          col_md_4.classList.add("col-xs-4");
+          topThreeNeeds.appendChild(col_md_4);
+          //the rest of needs go in 6 columns
+        } else {
+          var col_md_2 = document.createElement("div");
+          col_md_2.className = "col-md-2";
+          col_md_2.classList.add("col-sm-4", "col-xs-6");
+          restofNeeds.appendChild(col_md_2);
+        }
+        //create input element of type checkbox
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.id = name;
+        checkbox.name = "item";
+        checkbox.value = name;
 
-    //top three needs go in 3 columns
-    if (index <= 2){
-      var col_md_4 = document.createElement("div");
-      col_md_4.className = "col-md-4";
-      col_md_4.classList.add('col-xs-4');
-      topThreeNeeds.appendChild(col_md_4);
-    //the rest of needs go in 6 columns
-    }else{
-     var col_md_2 = document.createElement("div");
-     col_md_2.className = "col-md-2";
-     col_md_2.classList.add('col-sm-4', 'col-xs-6');
-     restofNeeds.appendChild(col_md_2);
-    }
-    //create input element of type checkbox
-    var checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.id = name;
-    checkbox.name = "item";
-    checkbox.value = name;
+        var label = document.createElement("label");
+        label.htmlFor = name;
 
-    var label = document.createElement("label");
-    label.htmlFor = name;
+        var img = document.createElement("img");
+        //default value when item not found is currently gift-card.svg
+        img.src =
+          "icons/" + (this.svgIcon[name.toLowerCase()] || "gift-card.svg");
 
-    var img = document.createElement("img");
-    //default value when item not found is currently gift-card.svg
-    img.src = "icons/" + (this.svgIcon[name.toLowerCase()] || "gift-card.svg");
+        //append elements for top three needs
+        if (index <= 2) {
+          col_md_4.appendChild(checkbox);
+          col_md_4.appendChild(label);
+          img.className = "img-check-main";
+          label.appendChild(img);
+        }
+        //append elements for the rest of the needs
+        else {
+          col_md_2.appendChild(checkbox);
+          col_md_2.appendChild(label);
+          img.className = "img-check";
+          label.appendChild(img);
+        }
 
-    //append elements for top three needs
-   if (index <= 2){
-    col_md_4.appendChild(checkbox);
-    col_md_4.appendChild(label);
-    img.className = "img-check-main";
-    label.appendChild(img); 
-   }
-   //append elements for the rest of the needs
-   else{
-    col_md_2.appendChild(checkbox);
-    col_md_2.appendChild(label);
-    img.className = "img-check";
-    label.appendChild(img); 
-   }
-     
-   var p = document.createElement("p");
-   p.className = "caption";
-   p.innerHTML = name;
-   label.appendChild(p);
-
-  }.bind(this));
-}
+        var p = document.createElement("p");
+        p.className = "caption";
+        p.innerHTML = name;
+        label.appendChild(p);
+      }.bind(this)
+    );
+  }
 };
 
 /*********** Global variables *********/
-var titleCase = function(string){
-  return string.replace(/\b\w/g, function(l){ return l.toUpperCase() });
-}
-
+var titleCase = function(string) {
+  return string.replace(/\b\w/g, function(l) {
+    return l.toUpperCase();
+  });
+};
 
 /*********** Initializating list of needs for index.html *********/
 controller.initNeeds();
--- web/js/results.js
+++ web/js/results.js
@@ -1,30 +1,30 @@
 /*********** Model *********/
 var results = {
   shelters: new ShelterList()
-}
+};
 
-function ShelterList(){
+function ShelterList() {
   this.shelters = [];
 }
 
-ShelterList.prototype.addShelter = function(name, wishlist, needs, website){
+ShelterList.prototype.addShelter = function(name, wishlist, needs, website) {
   this.shelters.push(new Shelter(name, wishlist, needs, website));
-}
+};
 
 //Sorts shelters in decreasing order based on number of items needed
-ShelterList.prototype.sortShelters = function(){
-  var filteredShelters = this.shelters.sort(function(s1, s2){
+ShelterList.prototype.sortShelters = function() {
+  var filteredShelters = this.shelters.sort(function(s1, s2) {
     return s1.needs.length > s2.needs.length;
   });
   this.shelters.reverse();
   return this.shelters;
-}
+};
 
-ShelterList.prototype.getShelters = function(){
- return this.shelters;
-}
+ShelterList.prototype.getShelters = function() {
+  return this.shelters;
+};
 
-function Shelter(name, wishlist, needs, website){
+function Shelter(name, wishlist, needs, website) {
   this.name = name;
   this.wishlist = wishlist;
   this.needs = needs;
@@ -33,172 +33,193 @@ function Shelter(name, wishlist, needs, website){
 
 /*********** Controller *********/
 var controller = {
- /*Will retrieve the items as needs from the url query string 
+  /*Will retrieve the items as needs from the url query string 
   and retrieve the shelters that need these items*/
-  getShelterResults: function(){
+  getShelterResults: function() {
     var needs = view.getURLParamsAll("item");
     view.showSelectedNeeds(needs);
     controller.retrieveShelters(needs);
   },
-  retrieveShelters: function(userSelections){
-    var url = 'http://www.maribelduran.com/Hacking4Humanity2017/web/js/ShelterDictionary.JSON';
-    this.getRequest(userSelections, url).then(function(response){
-      var shelters = JSON.parse(response);
-      shelters.forEach(function(obj){
-        var shelterNeeds = capitalizeNeeds(obj.needs);
-        var userSelectedNeeds = capitalizeNeeds(userSelections);
-        //check to see if there is at least one matching need 
-        if (findOne(shelterNeeds, userSelectedNeeds)){
-          // if there as least one matching need, find the interescting needs
-          var commonNeeds = userSelectedNeeds.filter(function(n){
+  retrieveShelters: function(userSelections) {
+    var url =
+      "https://maribelduran.github.io//Hacking4Humanity2017/web/js/ShelterDictionary.JSON";
+    this.getRequest(userSelections, url).then(
+      function(response) {
+        var shelters = JSON.parse(response);
+        shelters.forEach(function(obj) {
+          var shelterNeeds = capitalizeNeeds(obj.needs);
+          var userSelectedNeeds = capitalizeNeeds(userSelections);
+          //check to see if there is at least one matching need
+          if (findOne(shelterNeeds, userSelectedNeeds)) {
+            // if there as least one matching need, find the interescting needs
+            var commonNeeds = userSelectedNeeds.filter(function(n) {
               return shelterNeeds.indexOf(n) !== -1;
-          });
-          results.shelters.addShelter(obj.shelter, obj.wishList, commonNeeds, obj.website);
-        }
-      });
-      //sort and show shelters
-       results.shelters.sortShelters(); 
-       var shelterList = results.shelters.sortShelters(); 
-       view.postShelters(shelterList);
-    },function(error) {
-      console.error("Failed!", error);
-    })
+            });
+            results.shelters.addShelter(
+              obj.shelter,
+              obj.wishList,
+              commonNeeds,
+              obj.website
+            );
+          }
+        });
+        //sort and show shelters
+        results.shelters.sortShelters();
+        var shelterList = results.shelters.sortShelters();
+        view.postShelters(shelterList);
+      },
+      function(error) {
+        console.error("Failed!", error);
+      }
+    );
   },
-  getRequest:function(needs, url){
-    return new Promise(function(resolve,reject){
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.onload = function() {
-     if (request.status >= 200 && request.status < 400)  { // Success!
-        resolve(request.response);
-      }else{
-        if (request.status == 404){
-           resolve(request.response);
+  getRequest: function(needs, url) {
+    return new Promise(function(resolve, reject) {
+      var request = new XMLHttpRequest();
+      request.open("GET", url, true);
+      request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+          // Success!
+          resolve(request.response);
+        } else {
+          if (request.status == 404) {
+            resolve(request.response);
+          }
+          reject(Error(request.statusText)); // We reached our target server, but it returned an error
         }
-        reject(Error(request.statusText)); // We reached our target server, but it returned an error
-       }
       };
       request.onerror = function() {
-      reject(Error("Network Error"));  // There was a connection error of some sort
+        reject(Error("Network Error")); // There was a connection error of some sort
       };
-    request.send();
- });
-}
+      request.send();
+    });
+  }
 };
-
 
 /*********** View *********/
 var view = {
   /*Parameters for the Share Dialog in Facebook SDk*/
   fbShareDialogParams: {
-    method: 'share',
-    display: 'popup',
+    method: "share",
+    display: "popup",
     mobile_iframe: true,
-    href: 'http://www.maribelduran.com/Hacking4Humanity2017/web/index.html',
-    picture: 'https://raw.githubusercontent.com/maribelduran/Hacking4Humanity2017/master/web/images/EmpowerHerSF_square.png',
-    description: 'I am fighting against Human Trafficking by helping women shelters in SF. Come join the movement.',
-    caption: 'EMPOWERHER:SF',
-    quote: 'Be the change that you wish to see in the world.” -Gandhi',
- },
-  setUpEventListeners: function(){
-   var btn_share_fb=document.getElementById("btn-share-fb");
-   btn_share_fb.addEventListener("click", function(){
-      FB.ui(this.fbShareDialogParams, function(response) {});
-   }.bind(this));
+    href: "http://www.maribelduran.com/Hacking4Humanity2017/web/index.html",
+    picture:
+      "https://raw.githubusercontent.com/maribelduran/Hacking4Humanity2017/master/web/images/EmpowerHerSF_square.png",
+    description:
+      "I am fighting against Human Trafficking by helping women shelters in SF. Come join the movement.",
+    caption: "EMPOWERHER:SF",
+    quote: "Be the change that you wish to see in the world.” -Gandhi"
   },
-  getURLParamsAll: function(name){
-  var vars = [], hash;
-  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+  setUpEventListeners: function() {
+    var btn_share_fb = document.getElementById("btn-share-fb");
+    btn_share_fb.addEventListener(
+      "click",
+      function() {
+        FB.ui(this.fbShareDialogParams, function(response) {});
+      }.bind(this)
+    );
+  },
+  getURLParamsAll: function(name) {
+    var vars = [],
+      hash;
+    var hashes = window.location.href
+      .slice(window.location.href.indexOf("?") + 1)
+      .split("&");
 
-  for(var i = 0; i < hashes.length; i++){
-    hash = hashes[i].split('=');
-    if (hash[0] == name){
-     var item = decodeURIComponent(hash[1]).replace("+", " ");
-     vars.push(item);
+    for (var i = 0; i < hashes.length; i++) {
+      hash = hashes[i].split("=");
+      if (hash[0] == name) {
+        var item = decodeURIComponent(hash[1]).replace("+", " ");
+        vars.push(item);
+      }
+    }
+    return vars;
+  },
+  /*Creates elements to show shelters in each shelter-card.*/
+  postShelters: function(shelter) {
+    var shelterList = document.getElementById("shelter-list");
+    //clear current elements in the shelter-list div
+    shelterList.innerHTML = "";
+    shelter.forEach(function(s) {
+      var col_md_4 = document.createElement("div");
+      col_md_4.className = "col-md-4";
+      col_md_4.classList.add("col-sm-6");
+      shelterList.appendChild(col_md_4);
+
+      var shelterCard = document.createElement("div");
+      shelterCard.className = "shelter-card";
+      col_md_4.appendChild(shelterCard);
+
+      var wrapper = document.createElement("div");
+      wrapper.className = "wrapper";
+      shelterCard.appendChild(wrapper);
+
+      var cardImg = document.createElement("div");
+      cardImg.className = "card-img";
+      wrapper.appendChild(cardImg);
+
+      var card_img_src = document.createElement("img");
+      card_img_src.className = "card-img_src";
+      card_img_src.classList.add("center-block");
+      /*This will be updated in the future with corresponding shelter images */
+      card_img_src.src = "images/shelter.png";
+      cardImg.appendChild(card_img_src);
+
+      var h4 = document.createElement("h4");
+      h4.id = "shelterName";
+      h4.className = "name";
+      h4.innerHTML = s.name;
+      wrapper.appendChild(h4);
+
+      var p = document.createElement("p");
+      var needStr = "";
+      s.needs.forEach(function(item) {
+        needStr += item + ", ";
+      });
+      p.innerHTML = "Needs: " + needStr.substr(0, needStr.length - 2);
+      wrapper.appendChild(p);
+
+      var a = document.createElement("a");
+      a.href = s.website;
+      a.target = "_blank";
+      a.className = "btn";
+      a.classList.add("donateBtn");
+      a.innerHTML = "Donate";
+      wrapper.appendChild(a);
+    });
+  },
+  showSelectedNeeds: function(needs) {
+    if (needs[0] !== "undefined" && needs[0] !== undefined) {
+      document.getElementById("shelterNeedsSelection").innerHTML = needs.join(
+        ", "
+      );
+    } else {
+      document.getElementById("description").innerHTML =
+        "You did not select any items.";
     }
   }
-  return vars;
- },
- /*Creates elements to show shelters in each shelter-card.*/
- postShelters: function(shelter){
-  var shelterList = document.getElementById("shelter-list");
-  //clear current elements in the shelter-list div
-  shelterList.innerHTML = ""; 
-  shelter.forEach(function(s){
-   
-    var col_md_4 = document.createElement("div");
-    col_md_4.className = "col-md-4";
-    col_md_4.classList.add('col-sm-6');
-    shelterList.appendChild(col_md_4); 
-
-    var shelterCard = document.createElement("div");
-    shelterCard.className = "shelter-card";
-    col_md_4.appendChild(shelterCard); 
-
-    var wrapper = document.createElement("div");
-    wrapper.className = "wrapper";
-    shelterCard.appendChild(wrapper); 
-
-    var cardImg = document.createElement("div");
-    cardImg.className = "card-img";
-    wrapper.appendChild(cardImg); 
-
-    var card_img_src = document.createElement("img");
-    card_img_src.className = "card-img_src";
-    card_img_src.classList.add('center-block');
-    /*This will be updated in the future with corresponding shelter images */
-    card_img_src.src = "images/shelter.png"
-    cardImg.appendChild(card_img_src); 
-
-    var h4 = document.createElement("h4");
-    h4.id = "shelterName"
-    h4.className = "name";
-    h4.innerHTML = s.name;
-    wrapper.appendChild(h4); 
-
-    var p = document.createElement("p");
-    var needStr = "";
-    s.needs.forEach(function(item){
-        needStr += item + ", ";
-    });
-    p.innerHTML = "Needs: " + needStr.substr(0,needStr.length-2);
-    wrapper.appendChild(p); 
-
-    var a = document.createElement("a");
-    a.href = s.website;
-    a.target  = "_blank";
-    a.className = "btn";
-    a.classList.add('donateBtn');
-    a.innerHTML ="Donate"
-    wrapper.appendChild(a); 
- });
-},
-showSelectedNeeds: function(needs){
-  if( needs[0] !== "undefined" && needs[0] !== undefined){
-    document.getElementById("shelterNeedsSelection").innerHTML = needs.join(', ');
-  }else{
-     document.getElementById("description").innerHTML = "You did not select any items.";
-  }
- }
 };
 
 /*********** Global functions *********/
-var findOne = function (haystack, arr) {
-  return arr.some(function (v) {
+var findOne = function(haystack, arr) {
+  return arr.some(function(v) {
     return haystack.indexOf(v) >= 0;
-    });
+  });
 };
 
-var capitalizeNeeds = function(needs){
-  var titledCase =  needs.map(function(str){
-      return str.replace(/\b\w/g, function(l){ return l.toUpperCase() });
+var capitalizeNeeds = function(needs) {
+  var titledCase = needs.map(function(str) {
+    return str.replace(/\b\w/g, function(l) {
+      return l.toUpperCase();
+    });
   });
   return titledCase;
-}
+};
 
 /*********** Runs when document is ready *********/
-$(document).ready(function () {
-  controller. getShelterResults();
+$(document).ready(function() {
+  controller.getShelterResults();
   view.setUpEventListeners();
   $("#myModal").modal("show");
 });
